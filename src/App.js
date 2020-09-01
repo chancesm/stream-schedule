f84648cf14d97ae3cfe8cce5d27f593efeffa412
Chance Murray <chance.murray@domo.com>
2020-08-31 18:32:36 -0600

    first changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log("test");
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <img src="logo.png" alt=""></img>
+        <div className="separator"></div>
+        <div className="title">
+          Stream Schedule
+        </div>
+      </div>
+      <div className="schedule">
+
+      </div>
     </div>
   );
 }
