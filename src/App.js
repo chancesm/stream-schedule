2cba637eb0e2109e96531e40c0dcecaf64d73156
Chance Murray <chance.murray@domo.com>
2020-08-31 22:08:07 -0600

    first draft complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [schedule, setSchedule] = useState([]);
   useEffect(() => {
-    console.log("test");
+    fetch('/.netlify/functions/getSchedule').then(response => response.json()).then(data => setSchedule(data));
   }, [])
   return (
     <div className="App">
@@ -13,8 +14,24 @@ function App() {
           Stream Schedule
         </div>
       </div>
-      <div className="schedule">
-
+      <div className="bottom">
+        <div className="schedule">
+          <div className="days">
+            <div className="day">MONDAY</div>
+            <div className="day">TUESDAY</div>
+            <div className="day">WEDNESDAY</div>
+            <div className="day">THURSDAY</div>
+            <div className="day">FRIDAY</div>
+          </div>
+          <div className="content">
+            {schedule.map(item => (
+              <div key={item.id} className="scheduleItem" style={{width: `${224 * (item.fields.Size ?? 1)}px`}}>
+                <div className="eventTitle">{item.fields.Title}</div>
+                <div className="description">{item.fields.Description}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
